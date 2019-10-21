e23e7beef46adad910029a411328f5daad7e76c4
unknown <hernan_rodriguez_2013@hotmail.com>
2019-10-21 17:06:07 -0300

    Agregando botones

--- FINTER/FINTER/Form1.Designer.cs
+++ FINTER/FINTER/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lagrange";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(237, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Newton Gregory Progresivo";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(312, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(236, 44);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Newton Gregory Regresivo";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(584, 278);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
