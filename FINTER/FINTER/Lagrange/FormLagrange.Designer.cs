6eba60d0d9799b91a69883ce580e266c2c9b3a42
unknown <hernan_rodriguez_2013@hotmail.com>
2019-10-24 15:31:16 -0300

    Especializacion en k para lagrange

--- FINTER/FINTER/Lagrange/FormLagrange.Designer.cs
+++ FINTER/FINTER/Lagrange/FormLagrange.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.PolinomioResultante = new System.Windows.Forms.Label();
             this.MostrarPasos = new System.Windows.Forms.Button();
+            this.Especializador = new System.Windows.Forms.Button();
+            this.ingresarK = new System.Windows.Forms.TextBox();
+            this.Especializacion = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // PolinomioResultante
             // 
             this.PolinomioResultante.AutoSize = true;
-            this.PolinomioResultante.Location = new System.Drawing.Point(92, 23);
+            this.PolinomioResultante.Location = new System.Drawing.Point(24, 23);
             this.PolinomioResultante.Name = "PolinomioResultante";
             this.PolinomioResultante.Size = new System.Drawing.Size(106, 13);
             this.PolinomioResultante.TabIndex = 0;
@@ -44,7 +47,7 @@
             // 
             // MostrarPasos
             // 
-            this.MostrarPasos.Location = new System.Drawing.Point(86, 62);
+            this.MostrarPasos.Location = new System.Drawing.Point(18, 62);
             this.MostrarPasos.Name = "MostrarPasos";
             this.MostrarPasos.Size = new System.Drawing.Size(112, 23);
             this.MostrarPasos.TabIndex = 1;
@@ -52,11 +55,40 @@
             this.MostrarPasos.UseVisualStyleBackColor = true;
             this.MostrarPasos.Click += new System.EventHandler(this.MostrarPasos_Click);
             // 
+            // Especializador
+            // 
+            this.Especializador.Location = new System.Drawing.Point(189, 61);
+            this.Especializador.Name = "Especializador";
+            this.Especializador.Size = new System.Drawing.Size(84, 23);
+            this.Especializador.TabIndex = 2;
+            this.Especializador.Text = "Especializar K";
+            this.Especializador.UseVisualStyleBackColor = true;
+            this.Especializador.Click += new System.EventHandler(this.Especializador_Click);
+            // 
+            // ingresarK
+            // 
+            this.ingresarK.Location = new System.Drawing.Point(293, 63);
+            this.ingresarK.Name = "ingresarK";
+            this.ingresarK.Size = new System.Drawing.Size(73, 20);
+            this.ingresarK.TabIndex = 3;
+            // 
+            // Especializacion
+            // 
+            this.Especializacion.AutoSize = true;
+            this.Especializacion.Location = new System.Drawing.Point(387, 69);
+            this.Especializacion.Name = "Especializacion";
+            this.Especializacion.Size = new System.Drawing.Size(36, 13);
+            this.Especializacion.TabIndex = 4;
+            this.Especializacion.Text = "P(K) =";
+            // 
             // FormLagrange
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(525, 261);
+            this.Controls.Add(this.Especializacion);
+            this.Controls.Add(this.ingresarK);
+            this.Controls.Add(this.Especializador);
             this.Controls.Add(this.MostrarPasos);
             this.Controls.Add(this.PolinomioResultante);
             this.Name = "FormLagrange";
@@ -71,5 +103,8 @@
 
         private System.Windows.Forms.Label PolinomioResultante;
         private System.Windows.Forms.Button MostrarPasos;
+        private System.Windows.Forms.Button Especializador;
+        private System.Windows.Forms.TextBox ingresarK;
+        private System.Windows.Forms.Label Especializacion;
     }
 }
